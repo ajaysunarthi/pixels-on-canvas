1ce8843a07068fe7ef136eb9da3efcbb6e51e575
ajay <ajaysunarthi@iitj.ac.in>
2017-01-09 07:31:33 +0530

    create image object

--- script.js
+++ script.js
@@ -4,4 +4,28 @@ Array.prototype.forEach.call(document.querySelectorAll('.i'), function(Elm) {
 
 function process(imgEl) {
 	//processing
+	var pixArr = getPixels(imgEl);
+    var filter = imgEl.dataset.filter;
+
+    obj = {
+        pixel: pixArr,
+        filter: filter
+    };
+
+    console.log(obj);
+
+    function getPixels(img) {
+        var canvas, context;
+        canvas = getCanvas(img.width, img.height);
+        context = canvas.getContext('2d');
+        context.drawImage(img, 0, 0);
+        return context.getImageData(0, 0, canvas.width, canvas.height);
+    }
+
+    function getCanvas(width, height) {
+        var canvas = document.createElement('canvas');
+        canvas.width = width;
+        canvas.height = height;
+        return canvas;
+    }
 }
